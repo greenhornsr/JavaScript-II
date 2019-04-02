9682324c822f558e95468f535956e5221fa512c0
greenhornsr <srollins11279@gmail.com>
2019-04-02 12:59:33 -0700

    completed HOF, but not stretch yet

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,6 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+
 
 /* 
 
@@ -23,29 +23,64 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   });
 
 */
+const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+    return cb(arr.length);
 }
+  // HOF getLength invoked
+    getLength(items, function(myLength){
+      console.log(myLength);
+    })
+
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length-1])
 }
+  // HOF last invoked
+  last(items, function(finalItem){
+    console.log(finalItem);
+  })
+
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y)
 }
+// HOF sumNums invoked
+  sumNums(10, 15, function(finalSum){
+    console.log(finalSum);
+  })
+
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y)
 }
+  // HOF multiplyNums invoked
+  multiplyNums(10, 15, function(product){
+    console.log(product)
+  })
 
-function contains(item, list, cb) {
+function contains(items, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(list.includes(items))
 }
-
+  // HOF contains invoked
+  // true test
+    contains("Pencil", items, function(tfTest){
+      console.log(tfTest);
+    })
+  // false test
+  contains("Ruler", items, function(tfTest){
+    console.log(tfTest);
+  })
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
