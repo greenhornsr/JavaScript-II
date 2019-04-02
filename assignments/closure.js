18e43a45dc095753974606a66d2b2a1189020625
greenhornsr <srollins11279@gmail.com>
2019-04-02 14:14:22 -0700

    finished primary closure challenge

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,11 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+const mumble = "#I$* KD*#"
 
+function whatTheySay(){
+  console.log(`I wish I could understand what they say when they say "${mumble}"`);
+}
+console.log(whatTheySay());
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
